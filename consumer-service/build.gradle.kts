a7ccef59170fc63bb11db7184396801dfe300dd8
Eric França <efseric@gmail.com>
2024-12-29 01:25:12 -0300

    Commit disponibiliza projeto Java apos termino do curso de RabbitMQ

---
+++ consumer-service/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.4"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "br.com.frasil"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.rabbitmq:commons:1.0.0")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.amqp:spring-rabbit-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ producer-service/src/main/java/br/com/fransil/producer_service/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package br.com.fransil.producer_service.controllers;
+
+
+import br.com.fransil.producer_service.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    private final ProductService service;
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> produces(@RequestBody ProductDTO dto) {
+        service.createProduct(dto);
+        return ResponseEntity.status(CREATED).build();
+    }
+
+}
